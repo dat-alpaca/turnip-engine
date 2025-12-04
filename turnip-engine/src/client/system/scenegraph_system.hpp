82690cdeb7b374b8bde806f0c501d630cd63f4ec
dat-alpaca <paulocastrogalvao@gmail.com>
2025-12-05 01:28:41 +0900

    Removes zombie code

--- turnip-engine/src/client/system/scenegraph_system.hpp
+++ turnip-engine/src/client/system/scenegraph_system.hpp
@@ -30,56 +30,6 @@ namespace tur
 				else
 					transform.worldTransform = transform.transform;
 			}
-
-			return;
-
-			/*
-			// Resize entity level list:
-			u64 maxDepth = 0;
-			std::vector<std::vector<Entity>> entities;
-
-			auto view0 = rScene->get_registry().view<HierarchyComponent>();
-			for (auto [e, hierarchy] : view0.each())
-			{
-				if (hierarchy.level > maxDepth)
-					maxDepth = hierarchy.level;
-			}
-
-			entities.resize(maxDepth + 1);
-
-			// Get all entities based on level:
-			auto view1 = rScene->get_registry().view<TransformComponent, HierarchyComponent>();
-			for (auto [e, transform, hierarchy] : view1.each())
-				entities[hierarchy.level].push_back({e, rScene});
-
-			// Iterate through levels:
-			u64 level = 0;
-			for (auto& levelEntities : entities)
-			{
-				for (auto& entity : levelEntities)
-				{
-					auto& transform = entity.get_component<TransformComponent>();
-					auto& hierarchy = entity.get_component<HierarchyComponent>();
-
-					if (level != 0)
-					{
-						Entity parent = Entity{hierarchy.parent, rScene};
-						auto& parentTransform = parent.get_component<TransformComponent>();
-
-						transform.worldTransform = parentTransform.worldTransform * transform.transform;
-						TUR_LOG_WARN(
-							"{} {} {}", transform.worldTransform.position.x, transform.worldTransform.position.y,
-							transform.worldTransform.position.z
-						);
-					}
-					else
-					{
-					}
-				}
-				++level;
-			}
-
-			*/
 		}
 
 	private:
