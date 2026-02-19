a92eb3062c2959a758e13e88fe68796bd4f74b47
dat-alpaca <paulocastrogalvao@gmail.com>
2026-02-20 01:37:54 +0900

    Fixes deploy command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,10 @@
 set -e
 
 mkdir -p deploy
+mkdir -p deploy/turnip-vk
+mkdir -p deploy/turnip-gl
 
-cp -r res/ deploy/
-cp build/gl/turnip-sandbox/turnip-sandbox deploy/turnip-sandbox-gl
-cp build/vk/turnip-sandbox/turnip-sandbox deploy/turnip-sandbox-vk
+cp -r res/ deploy/turnip-gl
+cp -r res/ deploy/turnip-vk
+cp build/turnip-gl/turnip-sandbox/turnip-sandbox deploy/turnip-gl/turnip-sandbox-gl
+cp build/turnip-vk/turnip-sandbox/turnip-sandbox deploy/turnip-vk/turnip-sandbox-vk
