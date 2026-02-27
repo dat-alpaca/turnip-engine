28e498d67c2e01773e4b5cb9293039af14b7243f
dat-alpaca <paulocastrogalvao@gmail.com>
2026-02-27 16:25:02 +0900

    [WIP] Stops using json archives

--- turnip-engine/src/bridge/serialization/scene.hpp
+++ turnip-engine/src/bridge/serialization/scene.hpp
@@ -1,120 +1,56 @@
 #pragma once
 #include <json.hpp>
 #include <string>
-#include <type_traits>
-#include <unordered_map>
 
 #include "defines.hpp"
 #include "components.hpp"
-#include "entt/entity/entity.hpp"
 #include "entt/entity/fwd.hpp"
-#include "logging/logging.hpp"
 #include "utils/json/json_file.hpp"
 #include "bridge/project/project.hpp"
 
 namespace tur
 {
-    class JSONOutputArchive
+    template<typename T>
+    requires NamedComponent<T>
+    inline void serialize(entt::registry& registry, entt::entity entity, nlohmann::json& object)
     {
-    public:
-        void operator()(entt::entity entity)
-        {
-            if(mEntityList.size() != mEntityCount)
-            {
-                mEntityList.push_back(entity);
-                return;
-            }
-
-            if(mEntityMap.find(entity) == mEntityMap.end())
-                mEntityMap[entity] = {};
-
-            mEntityMap[entity].push_back({});
-            mCurrentComponentName = &mEntityMap[entity].back().first;
-            mCurrentValue = &mEntityMap[entity].back().second;
-        }
-
-        void operator()(std::underlying_type_t<entt::entity> size)
-        {
-            // Header:
-            if(mEntityCount == invalid_handle)
-            {
-                mEntityCount = size;
-                mEntityList.reserve(size);
-                return;
-            }
-        }
-
-        template<typename T>
-        requires NamedComponent<T>
-        void operator()(const T& component)
-        {
-            *mCurrentComponentName = T::get_component_name();
-            *mCurrentValue = component;
-        }
-
-    public:
-        void save(const std::filesystem::path& filepath)
-        {
-            nlohmann::json output;
-            output["entities"] = mEntityList;
-
-            nlohmann::json entityList = nlohmann::json::object();
-            for(auto& [entity, componentPair] : mEntityMap)
-            {
-                std::string key = std::to_string(static_cast<u32>(entity));
-                
-                nlohmann::json entityObject = nlohmann::json::object();
-                for(const auto& [componentName, component] : componentPair)
-                    entityObject[componentName] = component;
-
-                entityList[key] = entityObject;
-            }
-
-            output["components"] = entityList;
-            write_json(filepath, output);
-        }
-
-    private:
-        u32 mEntityCount = invalid_handle;
-        std::vector<entt::entity> mEntityList;
-
-        nlohmann::json* mCurrentValue = nullptr;
-        std::string* mCurrentComponentName = nullptr;
-
-        std::unordered_map<
-            entt::entity, 
-            std::vector<std::pair<std::string, nlohmann::json>>
-        > mEntityMap;
-    };
+        object[T::get_component_name()] = registry.get<T>(entity);
+    }
 
     inline void serialize_scene(NON_OWNING Scene* scene, const std::filesystem::path& filepath)
     {
-        JSONOutputArchive archive;
-        entt::snapshot{scene->get_registry()}
-            .get<entt::entity>(archive)
-            .get<AudioSourceComponent>(archive)
-            .get<CameraComponent>(archive)
-            .get<HierarchyComponent>(archive)
-            .get<UUIDComponent>(archive)
-            .get<NameComponent>(archive)
-            .get<TransformComponent>(archive)
-            .get<Sprite2DComponent>(archive)
-            .get<CubemapComponent>(archive)
-            .get<MeshComponent>(archive)
-            .get<Tilemap2DComponent>(archive)
-            .get<TextComponent>(archive)
-            .get<Body2DComponent>(archive)
-            .get<RectCollider2D>(archive)
-            .get<ScriptComponent>(archive);
+        // TODO: test
 
-        archive.save(filepath);
+        auto& registry = scene->get_registry();
+        nlohmann::json fileObject = nlohmann::json::object();
+
+        for(auto entity : scene->get_registry().view<entt::entity>())
+        {
+            auto entityKey = std::to_string(static_cast<u32>(entity));
+            fileObject[entityKey] = nlohmann::json::object();
+            auto& entityObject = fileObject[entityKey];
+
+            serialize<UUIDComponent>(registry, entity, entityObject);
+            serialize<NameComponent>(registry, entity, entityObject);
+            serialize<HierarchyComponent>(registry, entity, entityObject);
+            serialize<TransformComponent>(registry, entity, entityObject);
+            serialize<Body2DComponent>(registry, entity, entityObject);
+            serialize<RectCollider2D>(registry, entity, entityObject);
+            serialize<AudioSourceComponent>(registry, entity, entityObject);
+            serialize<CameraComponent>(registry, entity, entityObject);
+            serialize<Sprite2DComponent>(registry, entity, entityObject);
+            serialize<CubemapComponent>(registry, entity, entityObject);
+            serialize<MeshComponent>(registry, entity, entityObject);
+            serialize<Tilemap2DComponent>(registry, entity, entityObject);
+            serialize<TextComponent>(registry, entity, entityObject);
+            serialize<ScriptComponent>(registry, entity, entityObject);
+        }
     }
 
     template<typename T>
     requires NamedComponent<T>
-    inline void deserialize(NON_OWNING Scene* scene, const nlohmann::json& object)
+    inline void deserialize(entt::registry& registry, const nlohmann::json& object)
     {
-        auto& registry = scene->get_registry();
         std::string key = T::get_component_name();
         
         for(auto& [entityName, components] : object.items())
@@ -139,20 +75,19 @@ namespace tur
         for(auto& [entityName, _] : fileObject.items())
             entt::entity entity = registry.create(static_cast<entt::entity>(std::stoul(entityName)));
 
-
-        deserialize<UUIDComponent>(scene, fileObject);
-        deserialize<NameComponent>(scene, fileObject);
-        deserialize<HierarchyComponent>(scene, fileObject);
-        deserialize<TransformComponent>(scene, fileObject);
-        deserialize<Body2DComponent>(scene, fileObject);
-        deserialize<RectCollider2D>(scene, fileObject);
-        deserialize<AudioSourceComponent>(scene, fileObject);
-        deserialize<CameraComponent>(scene, fileObject);
-        deserialize<Sprite2DComponent>(scene, fileObject);
-        deserialize<CubemapComponent>(scene, fileObject);
-        deserialize<MeshComponent>(scene, fileObject);
-        deserialize<Tilemap2DComponent>(scene, fileObject);
-        deserialize<TextComponent>(scene, fileObject);
-        deserialize<ScriptComponent>(scene, fileObject);
+        deserialize<UUIDComponent>(registry, fileObject);
+        deserialize<NameComponent>(registry, fileObject);
+        deserialize<HierarchyComponent>(registry, fileObject);
+        deserialize<TransformComponent>(registry, fileObject);
+        deserialize<Body2DComponent>(registry, fileObject);
+        deserialize<RectCollider2D>(registry, fileObject);
+        deserialize<AudioSourceComponent>(registry, fileObject);
+        deserialize<CameraComponent>(registry, fileObject);
+        deserialize<Sprite2DComponent>(registry, fileObject);
+        deserialize<CubemapComponent>(registry, fileObject);
+        deserialize<MeshComponent>(registry, fileObject);
+        deserialize<Tilemap2DComponent>(registry, fileObject);
+        deserialize<TextComponent>(registry, fileObject);
+        deserialize<ScriptComponent>(registry, fileObject);
     }
 }
